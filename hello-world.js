d0a546fe03331cc7f42346982c4084f9a52af72e
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-16 20:13:29 +0000

    initiated iss file

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,19 @@
 //Monday Jan 16, 2017
 
 console.log("Hello world!");
+
+//pause console.log
+
+console.log("Hello World!");
+setTimeout(function() {
+	console.log("Hello World Again!");
+}, 10000);
+
+//recursion
+
+function go () {
+    console.log("Hello World Again!");
+    setTimeout(go,1000);
+}
+go();
+
